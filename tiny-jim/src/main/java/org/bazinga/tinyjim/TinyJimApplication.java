d057abe7a511152fdb14e51d4eced21768472383
liguolin <liguolin@>
2020-09-05 21:45:00 +0800

    init java module

---
+++ tiny-jim/src/main/java/org/bazinga/tinyjim/TinyJimApplication.java
@@ -0,0 +1,13 @@
+package org.bazinga.tinyjim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyJimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyJimApplication.class, args);
+    }
+
+}
---
+++ tiny-jim/src/test/java/org/bazinga/tinyjim/TinyJimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bazinga.tinyjim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyJimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
